e415d9f7caabc15a1ebc032ed8359c25896000b8
Cyanox <Infernogaming621@gmail.com>
2020-05-30 14:23:55 -0400

    Add project files.

---
+++ DoorRestartSystem/EventHandlers.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using EXILED;
+using EXILED.Extensions;
+using MEC;
+using UnityEngine;
+
+namespace DoorRestartSystem
+{
+	class EventHandlers
+	{
+		private CoroutineHandle coroutine;
+		private List<Door> fuckyDoors = new List<Door>();
+		private List<Door> doors;
+		private bool isRestarting = false;
+		private bool isRoundStarted = false;
+
+		private const float delay = 15.03f;
+
+		public void OnRoundRestart()
+		{
+			Timing.KillCoroutines(coroutine);
+			fuckyDoors.Clear();
+			isRestarting = false;
+			isRoundStarted = false;
+		}
+
+		public void OnRoundStart()
+		{
+			isRoundStarted = true;
+			coroutine = Timing.RunCoroutine(StartSystem());
+		}
+
+		public void OnRoundEnd() => isRoundStarted = false;
+
+		private IEnumerator<float> FuckDoor(Door door)
+		{
+			doors.Remove(door);
+			fuckyDoors.Add(door);
+			yield return Timing.WaitForSeconds(0.7f);
+			if (isRestarting)
+			{
+				door.NetworkisOpen = !door.NetworkisOpen;
+				door.Networklocked = !door.Networklocked;
+			}
+			doors.Add(door);
+			fuckyDoors.Remove(door);
+		}
+
+		private IEnumerator<float> StartSystem()
+		{
+			while (isRoundStarted)
+			{
+				yield return Timing.WaitForSeconds(UnityEngine.Random.Range(480, 660));
+				if (UnityEngine.Random.Range(0, 100) < 50)
+				{
+					doors = Map.Doors;
+					if (!Map.IsNukeInProgress && !Map.IsNukeDetonated)
+					{
+						isRestarting = true;
+						Timing.CallDelayed(delay, () => isRestarting = false);
+						Cassie.CassieMessage("CRITICAL ERROR . . DOOR SYSTEM MALFUNCTION IN PROGRESS . . DOOR SYSTEM SOFTWARE REPAIR COMMENCING IN 3 . 2 . 1 . . . . . . . DOOR SYSTEM REPAIR COMPLETE", true, true);
+						List<Door> openDoors = new List<Door>();
+						foreach (Door door in Map.Doors) if (door.NetworkisOpen) openDoors.Add(door);
+						while (isRestarting)
+						{
+							Door door = doors[UnityEngine.Random.Range(0, doors.Count)];
+							Timing.RunCoroutine(FuckDoor(door));
+							yield return Timing.WaitForSeconds(0.05f);
+						}
+						foreach (Door door in Map.Doors)
+						{
+							door.NetworkisOpen = false;
+							door.Networklocked = true;
+						}
+						yield return Timing.WaitForSeconds(3f);
+						foreach (Door door in Map.Doors)
+						{
+							door.NetworkisOpen = openDoors.Contains(door);
+							door.Networklocked = false;
+						}
+						fuckyDoors.Clear();
+					}
+				}
+			}
+		}
+	}
+}
---
+++ DoorRestartSystem/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using EXILED;
+
+namespace DoorRestartSystem
+{
+	public class Plugin : EXILED.Plugin
+	{
+		private EventHandlers ev;
+
+		public override void OnEnable()
+		{
+			ev = new EventHandlers();
+			Events.RoundRestartEvent += ev.OnRoundRestart;
+			Events.RoundStartEvent += ev.OnRoundStart;
+			Events.RoundEndEvent += ev.OnRoundEnd;
+		}
+
+		public override void OnDisable() { }
+
+		public override void OnReload() { }
+
+		public override string getName { get; } = "DoorRestartSystem";
+	}
+}
